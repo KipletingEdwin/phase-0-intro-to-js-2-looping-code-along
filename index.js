6e55232bd4c33f40cef532ec92c1173f17a290c5
KipletingEdwin <kipletingedwin4@gmail.com>
2022-06-03 18:25:03 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(arrayName, event){
+    let thankCard = []
+    for(let i = 0; i < arrayName.length; i++){
+         let message =`Thank you, ${arrayName[i]}, for the wonderful ${event} gift!`
+         thankCard.push(message)
+    }
+    return thankCard
+}
+function countDown(count){
+    while(count >= 0){
+        console.log(count--)
+    }
+}
